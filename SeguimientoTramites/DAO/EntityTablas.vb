60871a90cadb0fad73cc12c05875904d7050252e
rpach17 <rpach17@505ce38a-ec38-94ee-6166-4b33904502be>
2014-06-11 19:20:08 +0000

    left join

--- SeguimientoTramites/DAO/EntityTablas.vb
+++ SeguimientoTramites/DAO/EntityTablas.vb
@@ -113,10 +113,12 @@
 
                 Dim usuarios = (From u In ctx.USUARIOS
                                 Join s In ctx.SALTOS On s.IDPUESTO Equals u.PUESTO.IDPUESTO
-                                Join dt In ctx.DETALLE_TRAMITE On dt.IDUSUARIO Equals u.IDUSUARIO
+                                Group Join dt In ctx.DETALLE_TRAMITE On dt.IDUSUARIO Equals u.IDUSUARIO
+                                Into udt = Group
+                                From dt In udt.DefaultIfEmpty()
                                 Where s.IDSALTO = PasoTramite And dt.FECHA_ENTREGA Is Nothing And Not dt.DESTINO = 0
                                 Group By u.NOMBRE, u.APELLIDOS
-                                Into TotalTramites = Count()
+                Into TotalTramites = Count()
                                 Select NOMBRE, APELLIDOS, TotalTramites).tolist()
                 grid.DataSource = usuarios
             ElseIf destinos = 0 Then
