92e5f1d9478a5eb72c7ff98a60dc1e9e5e750dc4
josuetec2003 <josuetec2003@505ce38a-ec38-94ee-6166-4b33904502be>
2014-10-30 21:30:55 +0000

    Modelo de Archivos y Marcadores en Entity

---
+++ trunk/SeguimientoTramites/Formularios/frmDescargarDoc.Designer.vb
@@ -0,0 +1,56 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmDescargarDoc
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnGenerar = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnGenerar
+        '
+        Me.btnGenerar.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!)
+        Me.btnGenerar.Image = Global.SeguimientoTramites.My.Resources.Resources.document_check_compatibility
+        Me.btnGenerar.ImageAlign = System.Drawing.ContentAlignment.MiddleRight
+        Me.btnGenerar.Location = New System.Drawing.Point(145, 44)
+        Me.btnGenerar.Name = "btnGenerar"
+        Me.btnGenerar.Size = New System.Drawing.Size(230, 58)
+        Me.btnGenerar.TabIndex = 0
+        Me.btnGenerar.Text = "Generar documento"
+        Me.btnGenerar.UseVisualStyleBackColor = True
+        '
+        'frmDescargarDoc
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(519, 149)
+        Me.Controls.Add(Me.btnGenerar)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "frmDescargarDoc"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Generación de documento"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents btnGenerar As System.Windows.Forms.Button
+End Class
---
+++ trunk/SeguimientoTramites/Formularios/frmDescargarDoc.vb
@@ -0,0 +1,3 @@
+﻿Public Class frmDescargarDoc
+
+End Class
