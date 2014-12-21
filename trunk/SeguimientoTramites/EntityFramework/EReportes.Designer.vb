afb976813716e9ccd5106076164d2a73bfca4c50
rpach17@gmail.com <rpach17@gmail.com@505ce38a-ec38-94ee-6166-4b33904502be>
2014-12-21 22:23:59 +0000

    NUEVA ENTIDAD PARA REPORTES

---
+++ trunk/SeguimientoTramites/EntityFramework/EReportes.Designer.vb
@@ -0,0 +1,4 @@
+﻿' La generación de código predeterminada está deshabilitada para el modelo 'C:\Users\Administrator\Documents\Visual Studio 2010\Projects\Seguimiento Tramite0\SeguimientoTramites\EntityFramework\EReportes.edmx'. 
+' Para habilitar la generación de código predeterminada, cambie el valor de la propiedad del diseñador 'Estrategia de generación de código'
+' por otro valor. Esta propiedad está disponible en la ventana Propiedades cuando se abre
+' el modelo en el diseñador.
