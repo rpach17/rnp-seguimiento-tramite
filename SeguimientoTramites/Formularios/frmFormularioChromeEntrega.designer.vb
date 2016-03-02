be0296af05f852207fbc7c43bc8b11cc295aef23
rpach17 <rpach17@gmail.com>
2016-03-02 08:16:18 -0600

    todo bien

---
+++ SeguimientoTramites/Formularios/frmFormularioChromeEntrega.designer.vb
@@ -0,0 +1,68 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmFormularioChromeEntrega
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.WebControl1 = New Awesomium.Windows.Forms.WebControl(Me.components)
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.TextBox1.Location = New System.Drawing.Point(12, 367)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(735, 20)
+        Me.TextBox1.TabIndex = 3
+        Me.TextBox1.Visible = False
+        '
+        'WebControl1
+        '
+        Me.WebControl1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.WebControl1.Location = New System.Drawing.Point(0, 0)
+        Me.WebControl1.Size = New System.Drawing.Size(759, 353)
+        Me.WebControl1.TabIndex = 4
+        '
+        'frmFormularioChromeEntrega
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Silver
+        Me.ClientSize = New System.Drawing.Size(759, 399)
+        Me.Controls.Add(Me.WebControl1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "frmFormularioChromeEntrega"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Formulario"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Private WithEvents WebControl1 As Awesomium.Windows.Forms.WebControl
+End Class
---
+++ SeguimientoTramites/Formularios/frmFormularioChromeEntrega.vb
@@ -0,0 +1,54 @@
+﻿Public Class frmFormularioChromeEntrega
+
+    Dim idSalto As Integer
+    Public Property IdSalto1 As Integer
+        Get
+            Return idSalto
+        End Get
+        Set(ByVal value As Integer)
+            idSalto = value
+        End Set
+    End Property
+
+    Dim idForm As Integer
+    Public Property IdForm1 As Integer
+        Get
+            Return idForm
+        End Get
+        Set(ByVal value As Integer)
+            idForm = value
+        End Set
+    End Property
+
+    Dim idDetalleSeguimiento As Integer
+    Public Property IdDetalleSeguimiento1 As Integer
+        Get
+            Return idDetalleSeguimiento
+        End Get
+        Set(ByVal value As Integer)
+            idDetalleSeguimiento = value
+        End Set
+    End Property
+
+    Dim esEntrega As Boolean
+    Public Property EsEntrega1 As Boolean
+        Get
+            Return esEntrega
+        End Get
+        Set(ByVal value As Boolean)
+            esEntrega = value
+        End Set
+    End Property
+
+    Private Sub frmFormulario_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim url As String = String.Format("{0}?idf={1}&idu={2}&idds={3}&depto={4}&munic={5}&suc={6}&ofi={7}&entrega={8}", My.Settings.URLForm, idForm, 0, idDetalleSeguimiento, Replace(SesionActiva.Depto.ToLower, " ", "_"), Replace(SesionActiva.Municipio.ToLower, " ", "_"), Replace(SesionActiva.Sucursal.ToLower, " ", "_"), Replace(SesionActiva.Oficina.ToLower, " ", "_"), esEntrega)
+        WebControl1.Source = New Uri(url)
+        TextBox1.Text = url
+    End Sub
+
+    Private Sub TextBox1_KeyPress(sender As Object, e As KeyPressEventArgs) Handles TextBox1.KeyPress
+        If e.KeyChar = ChrW(13) Then
+            WebControl1.Source = New Uri(TextBox1.Text)
+        End If
+    End Sub
+End Class
